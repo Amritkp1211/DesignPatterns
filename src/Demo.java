0e178a1a2f7c6f2024c3779b3bbaf9bf5878b2c0
Amrit Kumar P <amritkp12112000@gmail.com>
2024-07-08 12:19:20 +0530

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Constructor;
+
+public class Demo {
+
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException {
+		
+		//singleton design pattern 
+		
+		
+//		Person p =Person.getPerson();
+//		
+//		System.out.println(p.hashCode());
+//		
+//		Person p1=Person.getPerson();
+//		
+//		System.out.println(p1.hashCode());
+//		
+//		System.out.println(Jalebi.getJalebi().hashCode());
+//		System.out.println(Jalebi.getJalebi().hashCode());
+		
+		//Person person = Person.getPerson();
+		//Person.class.getDeclaredConstructor
+		
+		
+	}
+	
+}
---
+++ src/Jalebi.java
@@ -0,0 +1,9 @@
+public class Jalebi {
+
+	private static Jalebi jalebi=new Jalebi();
+	
+	//eager 
+	public static Jalebi getJalebi() {
+		 return jalebi;
+	}
+}
